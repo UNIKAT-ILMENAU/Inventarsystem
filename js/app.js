c611dfe665d948f09e8f4e67ae7a2a4327c62b00
Kevin Bartsch <kevin.bartsch@gmx.net>
2016-06-09 20:07:45 +0200

    Public List Update
    TODO -
    detailview adjusting
    list adjusting

--- js/app.js
+++ js/app.js
@@ -3,24 +3,24 @@
 
 /* App Module */
 
-var invApp = angular.module('invApp', [
-  'ngRoute', 'invControllers', 'invServices'
-]);
+var invApp = angular.module('invApp', ['ngRoute', 'invControllers', 'invServices']);
 
+//==============================
 //Route definition
+//==============================
 invApp.config(['$routeProvider',  
   function($routeProvider) {
     $routeProvider.
       when('/', {
-        templateUrl: 'html/list.html',    //inventory public list  
+        templateUrl: 'html/list.html',    //public list route  
         controller: 'ListCtrl'
       }).
       when('/listData/:ListItemId', {
-        templateUrl: 'html/detail.html',  //selected item detailview
+        templateUrl: 'html/detail.html',  //public detailview route
         controller: 'DetailCtrl'
       }).
       otherwise({
-        redirectTo: '/'                    
+        redirectTo: '/'                                  
       });
   }]);
 
