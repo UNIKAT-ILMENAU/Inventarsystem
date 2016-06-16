7f215037fd1dc8100831d449dbd4b5e385df75cc
Franz Stecher <franz.stecher@gmx.net>
2016-06-16 19:33:30 +0200

    Auth Controller added and set some routes for postman testing

--- code/laravel/app/Http/Controllers/AuthenticateController.php
+++ code/laravel/app/Http/Controllers/AuthenticateController.php
@@ -6,74 +6,142 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Hash;
+use DB;
 
 //JWT Libaries
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTFactory;
+//use Tymon\JWTAuth\JWTManager;
 
 class AuthenticateController extends Controller
 {
-	/*
-    public function authenticate(Request $request)
+    
+    public function checkAuth(Request $request)
     {
-        // grab credentials from the request
-        $credentials = $request->only('email', 'password');
-        //return $credentials;
-        try {
-            // attempt to verify the credentials and create a token for the user
-            if (! $token = JWTAuth::attempt($credentials)) {
-				return response()->json(['error' => 'invalid_credentials'], 401);
-            }
-        } catch (JWTException $e) {
-            // something went wrong whilst attempting to encode the token
-            return response()->json(['error' => 'could_not_create_token'], 500);
+        try{
+            // try code
+            //$token = 'Bearer ' . JWTAuth::getToken();
+            $token = JWTAuth::getToken();
+            
+            $payload = JWTAuth::getPayload($token);
+            //return $payload; 
+
+            $manager = JWTAuth::manager();
+            $test = $manager->decode($token);
+
+            
+            //$test2 = $manager->getClaims();
+            //return response->json_decode($test);
+            return $test;
+        } 
+        catch(\Exception $e){
+            return response()->json(['error' => 'login failed. no or wrong token was founded.']);
         }
 
-        // all good so return the token
-        return response()->json(compact('token'));
+        /*
+        //echo "CheckAuth Route";
+        //Not so important
+        //$token = JWTAuth::parseToken();
+        //echo "Parse Header Authorization Token";
+        $token = JWTAuth::getToken();
+        
+        $payload = JWTAuth::getPayload($token);
+        //return $payload; 
+
+        $manager = JWTAuth::manager();
+        $test = $manager->decode($token);
+
+        
+        //$test2 = $manager->getClaims();
+        //return response->json_decode($test);
+        return $test;
+        */
     }
-	*/
+    
 
-    public function authenticate(Request $request)
+    public function createToken(Request $request)
     {
-    	$checkemail = false;
-    	$checkpassword = false;
+
+        //These vars will be true when the and the hashed password are valid
+        $checkemail = false;
+        $checkpassword = false;
 
 
 
-    	//Username Password Request == Datenbank username password
-    	$r_email = $request->input('email');
-    	$r_password = $request->input('password');
+        //Extract email and username from the request
+        $r_email = $request->input('email');
+        $r_password = $request->input('password');
 
-    	$password = Hash::make($r_password);
-    	return $password;
-    	/*
-    	$db_mail = DB::table('user')
+        //Testing Hashing the password
+        $password = Hash::make($request);
+        
+        //Try if the user email is in db 
+        $db_mail = DB::table('user')
             ->where('Email', $r_email)
             ->select('Email')
-            ->get();
+            ->pluck('Email');
+        
+        //when email was founded then change to true
+        if($db_mail[0] == $r_email)
+        {
+            $checkemail = true;
+        }
+
+        //search for member id in user table with the email address
+        $db_memberid = DB::table('user')
+            ->where('Email', $r_email)
+            ->select('member_id')
+            ->pluck('member_id');
+
+        //search for userid in DB with the email address
+        $db_userid = DB::table('user')
+            ->where('Email', $r_email)
+            ->select('id')
+            ->pluck('id');
+
+        //search for the password in member with the userid
+        $db_password = DB::table('member')
+            ->where('id', '=', $db_memberid[0])
+            ->select('password')
+            ->pluck('password');
+
+
+        if(Hash::check($r_password, $db_password[0]))
+        {
+            $checkpassword = true;
+        }
+
+
+        if($checkpassword&&$checkemail){
             
-    	if($db_mail == $r_email)
-    	{
-    		$checkemail = true;
-    	}
+            //Credentials are right!
+            $customClaims = ['User_Id' => $db_userid[0], 'User_Email' => $db_mail[0]];
+            $payload = JWTFactory::make($customClaims);
+            $token = JWTAuth::encode($payload);
+            
+            //send token back
+            return response()->json(['token' => (string)$token]);
 
-    	$db_password = DB::table('user')
-            ->where('Email', $r_email)
-            ->select('Email')
-            ->get();
+        }elseif (!$checkpassword) {
+            return response()->json(['error' => 'The Password is wrong']);
+        }elseif (!$checkemail) {
+            return response()->json(['error' => 'The Email is wrong']);
+        }elseif (!$$checkpassword&&!$checkemail) {
+            return response()->json(['error' => 'The Email and the password are wrong']);
+        }
 
-    	if($db_mail == $r_email)
-    	{
-    		$checkemail = true;
-    	}
+        /*
+        $customClaims = ['email' => $r_email, 'password' => $r_password];
+        $payload = JWTFactory::make($customClaims);
 
-    	$customClaims = ['email' => $r_email, 'password' => $r_password];
-		$payload = JWTFactory::make($customClaims);
+        $token = JWTAuth::encode($payload);
+        //return $token;
+        //serialize($token);
+        //return response()->json(JWTAuth::setToken($token));
+        //return response()->json(['token' => JWTAuth::getToken()]);
 
-		$token = JWTAuth::encode($payload);
-		return response()->json($token);
+        return gettype($token);
 
         */
     }
--- code/laravel/app/Http/routes.php
+++ code/laravel/app/Http/routes.php
@@ -97,6 +97,5 @@ Route::post('/api/v1/restricted/user/update/{id}', ['uses' =>'UserController@Use
 
 
 //JWT AUTH
-Route::post('/api/v1/login', ['uses' =>'AuthenticateController@authenticate']);
-
-
+Route::post('/api/v1/login', ['uses' =>'AuthenticateController@createToken']);
+Route::post('/api/v1/test', ['uses' =>'AuthenticateController@checkAuth']);
