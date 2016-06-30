8438390c069afd4f1a70be0eac9fc18ddf42c5dd
Max Ruetz <Maximilian.Ruetz@gmx.net>
2016-06-30 12:14:22 +0200

    added authcheck

--- admin/js/app.js
+++ admin/js/app.js
@@ -7,11 +7,11 @@ var invApp = angular.module('invApp', [
   'ngRoute', 'invControllers', 'invServices', 'invFilter'
 ]);
 
-//tokenInterceptor bugfix: 
-//injecting JWT Authentication in http-Header by using factory 
+//injecting JWT Authentication in http-Header by using factory.  
 invApp.config(['$httpProvider', function($httpProvider){
   $httpProvider.interceptors.push('tokenInterceptor');
 }]);
+
 
 //Route definition
 invApp.config(['$routeProvider',  
@@ -27,59 +27,73 @@ invApp.config(['$routeProvider',
       }).
       when('/dashboard', {                        //Dashboard - Mainmenu
         templateUrl: 'html/dashboard.html',
-        controller: 'MenuCtrl'
+        controller: 'MenuCtrl',
+        resolve: {function(authCheck){authCheck.check()}} 
       }).     
       when('/sysconf', {                          //Sidemenu - Systemconfiguration
         templateUrl: 'html/systemconf_menu.html',
-        controller: 'MenuCtrl'
+        controller: 'MenuCtrl',
+        resolve: {function(authCheck){authCheck.check()}} 
       }).
       when('/adminconf', {                        //Sidemenu - Adminconfiguration
         templateUrl: 'html/adminconf_menu.html',
-        controller: 'MenuCtrl'
+        controller: 'MenuCtrl',
+        resolve: {function(authCheck){authCheck.check()}} 
       }).
       when('/list', {                             //Itemlist
         templateUrl: 'html/itemlist/itemlist.html',    
-        controller: 'ListCtrl'
+        controller: 'ListCtrl',
+        resolve: {function(authCheck){authCheck.check()}} 
       }).
       when('/listData/:ListItemId', {             //Item detail view
         templateUrl: 'html/itemlist/detail.html',
-        controller: 'DetailCtrl'
+        controller: 'DetailCtrl',
+        resolve: {function(authCheck){authCheck.check()}} 
       }).
       when('/create_device', {                    //Create device
         templateUrl: 'html/itemlist/create_device.html', 
-        controller: 'CreateCtrl'
+        controller: 'CreateCtrl',
+        resolve: {function(authCheck){authCheck.check()}} 
       }).
       when('/create_material', {                  //Create material
         templateUrl: 'html/itemlist/create_material.html', 
-        controller: 'CreateCtrl'
+        controller: 'CreateCtrl',
+        resolve: {function(authCheck){authCheck.check()}} 
       }).
       when('/edit_item/:ListItemId', {            //Edit item view
         templateUrl: 'html/itemlist/edit_item.html',
-        controller: 'ItemEditCtrl'
+        controller: 'ItemEditCtrl',
+        resolve: {function(authCheck){authCheck.check()}}
       }).
       when('/rental', {                           //Rental form
         templateUrl: 'html/itemlist/rental.html',    
-        controller: 'RentalCtrl'
+        controller: 'RentalCtrl',
+        resolve: {function(authCheck){authCheck.check()}}
       }).
       when('/rentallist', {                       //All rented items list
         templateUrl: 'html/rentallist/rentallist.html',      
-        controller: 'RentalListCtrl'
+        controller: 'RentalListCtrl',
+        resolve: {function(authCheck){authCheck.check()}}
       }).   
       when('/rentalData/:ListItemId', {           //Rented item detail view
         templateUrl: 'html/rentallist/rental_detail.html',      
-        controller: 'RentalDetailCtrl'
+        controller: 'RentalDetailCtrl',
+        resolve: {function(authCheck){authCheck.check()}}
       }).
       when('/category', {                         //Category configuration
         templateUrl: 'html/systemconf/category.html',      
-        controller: 'CategoryCtrl'
+        controller: 'CategoryCtrl',
+        resolve: {function(authCheck){authCheck.check()}}
       }).   
       when('/place', {                            //Place configuration
         templateUrl: 'html/systemconf/place.html',      
-        controller: 'PlaceCtrl'
+        controller: 'PlaceCtrl',
+        resolve: {function(authCheck){authCheck.check()}}
       }).                                       
       when('/inviteAdmin', {                      //invite Admin form                
         templateUrl: 'html/adminconf/inviteAdmin.html',
-        controller: 'inviteAdminCtrl'
+        controller: 'inviteAdminCtrl',
+        resolve: {function(authCheck){authCheck.check()}}
       }).
       when('/createNewAdmin', {                   //create a new admin after inviting
         templateUrl: 'html/adminconf/createNewAdmin.html',
@@ -87,11 +101,13 @@ invApp.config(['$routeProvider',
       }).
       when('/resetPassword', {                    //change password when already logged in
         templateUrl: 'html/adminconf/resetPassword.html', 
-        controller: 'resetPasswordAsAdminCtrl'
+        controller: 'resetPasswordAsAdminCtrl',
+        resolve: {function(authCheck){authCheck.check()}}
       }).
       when('/deleteAdmin', {                      //delete an admin
         templateUrl: 'html/adminconf/deleteAdmin.html',
-        controller: 'deleteAdminCtrl'
+        controller: 'deleteAdminCtrl',
+        resolve: {function(authCheck){authCheck.check()}}
       }).
       when('/forgotPassword', {                   //change password when not logged in (forgot password)
         templateUrl: 'html/login/forgotPassword.html',
@@ -105,4 +121,3 @@ invApp.config(['$routeProvider',
         redirectTo: '/'
       });
   }]);
-
