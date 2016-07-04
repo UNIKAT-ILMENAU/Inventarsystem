4af898b9b9e596aa5f15276675af98638ecc695b
Max_Ruetz <Maximilian.Ruetz@gmx.net>
2016-07-04 13:08:24 +0200

    delete bc of error message bug

--- vagrant/vagrant_config/after.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# If you would like to do some extra provisioning you may
-# add any commands you wish to this file and they will
-# be run after the Homestead machine is provisioned.
-
-#hier steht dann mal der git clone für unseren code
-#composer create-project --prefer-dist laravel/laravel coden
