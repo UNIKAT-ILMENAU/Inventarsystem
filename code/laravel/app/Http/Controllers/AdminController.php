fa5bd695ccd16ac4c3f1abb6f3b9fc1cfa8928f5
Max_Ruetz <Maximilian.Ruetz@gmx.net>
2016-07-10 19:31:13 +0200

    passowrd change: error codes

--- code/laravel/app/Http/Controllers/AdminController.php
+++ code/laravel/app/Http/Controllers/AdminController.php
@@ -118,15 +118,6 @@ class AdminController extends Controller
         return 'Success';
     }
 
-
-    public function setPassword(Request $request, $id) 
-    {  
-        $R_oldpassword = $request->input('oldpassword');
-        $R_newpassword = $request->input('newpassword');
-
-        $checkpw = DB::table('user')->join('member', 'user.member_id', '=', 'member.id')->where('user.id', $id)->select('member.password')->get();
-    }
-
     public function invite(Request $request)
     {
    
@@ -246,10 +237,13 @@ class AdminController extends Controller
 
         //return $old_db->password;
 
-        if($oldpwcheck)
+        if($oldpwcheck == true)
         {
             DB::table('member')->where('id', '=', $member_id)
             ->update(array('password' => $r_password));            
+        }else
+        {
+        	return response()->json(['error' => 'Password was not changed']);
         }
 
         return response()->json(['message' => 'Password was changed']);
