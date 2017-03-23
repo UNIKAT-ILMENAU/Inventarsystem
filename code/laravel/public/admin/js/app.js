16d288b28f310521a0ff711f8124d5a7bd74b746
Dirk Heinke <dirkheinke@gmail.com>
2017-03-23 23:09:48 +0100

    Fix edit of material

--- code/laravel/public/admin/js/app.js
+++ code/laravel/public/admin/js/app.js
@@ -121,3 +121,17 @@ invApp.config(['$routeProvider',
         redirectTo: '/'
       });
   }]);
+
+invApp.directive('stringToNumber', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(value) {
+                return '' + value;
+            });
+            ngModel.$formatters.push(function(value) {
+                return parseFloat(value);
+            });
+        }
+    };
+});
