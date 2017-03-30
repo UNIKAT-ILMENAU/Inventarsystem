ec752cc9a8ef5ae0e9657e56daa4365b4bf0f46c
Dirk Heinke <dirkheinke@gmail.com>
2017-03-31 00:36:02 +0200

    Initial Data Setup

---
+++ code/laravel/app/Http/Controllers/MaterialController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Material;
+use Illuminate\Http\Request;
+
+class MaterialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Material $material)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Material $material)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Material $material)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Material $material)
+    {
+        //
+    }
+}
